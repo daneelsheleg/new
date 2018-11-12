bb6702ca6533a7df87df96796f1732460518ea6a
daneelsheleg <42346177+daneelsheleg@users.noreply.github.com>
2018-11-12 17:49:23 +0300

    Add files via upload

--- js.js
+++ js.js
@@ -31,8 +31,8 @@ document.addEventListener('DOMContentLoaded', function () {
     var input = prompt('меню');
 
     var ul = document.createElement('ul');
-        function generate(input) {
-            for(  var i = 0;  i < input; ){
+        var one = (function()  {
+            for(  var i = 1;  i-1 < input; ){
                 var li = document.createElement('li');
 
                 var p = document.createElement('p');
@@ -43,9 +43,8 @@ document.addEventListener('DOMContentLoaded', function () {
                 li.style = 'border:1px solid; border-radius:50%; width:100px; height:100px;display:inline-block;margin-bottom:10px; margin-right:10px;';
             }
     }
-
+())
     document.body.appendChild(ul);
-    generate(input);
 
 
 })
